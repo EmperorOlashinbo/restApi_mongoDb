3762f639d7cc9057cd48099e82f9b7a2222270d7
EmperorOlashinbo <ib.olasunbo@gmail.com>
2025-04-12 09:43:27 +0200

    updates

--- public/app.js
+++ public/app.js
@@ -17,4 +17,46 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Error fetcting dishes: ' + error.message);
         }
     }
-    
+
+    function displayDishes(dishes) {
+        dishesTableBody.innerHTML = '';
+
+        dishes.forEach(dish => {
+            const row = document.createElement('tr');
+
+            const nameCell = document.createElement('td');
+            nameCell.textContent = dish.name;
+            row.appendChild(nameCell);
+
+            const ingredientsCell = document.createElement('td');
+            ingredientsCell.textContent = dish.ingredients.join(', ');
+            row.appendChild(ingredientsCell);
+
+            const preparationsStepsCell = document.createElement('td');
+            preparationsStepsCell.textContent = dish.preparationsSteps.join(', ');
+            row.appendChild(preparationsStepsCell);
+
+            const cookingTimeCell = document.createElement('td');
+            cookingTimeCell.textContent = dish.cookingTime;
+            row.appendChild(cookingTimeCell);
+
+            const originCell = document.createElement('td');
+            originCell.textContent = dish.origin;
+            row.appendChild(originCell);
+
+            const spiceLevelCell = document.createElement('td');
+            spiceLevelCell.textContent = dish.spiceLevel;
+            row.appendChild(spiceLevelCell);
+
+            const actionsCell = document.createElement('td');
+
+            const updateButton = document.createElement('button');
+            updateButton.textContent = 'Update';
+            updateButton.onclick = () => editDish(dish._id);
+            actionsCell.appendChild(updateButton);
+
+            row.appendChild(actionsCell);
+
+            dishesTableBody.appendChild(row);
+        })
+    }
